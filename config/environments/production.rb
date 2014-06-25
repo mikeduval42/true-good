ef805824aae2b441072051cd61691c171b35b11f
michael duval <michaelduval@michaels-MacBook-Pro-2.local>
2014-06-24 21:16:30 -0700

    Fix asset attempt

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,8 +78,6 @@ Truegood::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.assets.js_compressor = Sprockets::LazyCompressor.new {
-    Uglifier.new(:mangle => false)
-  }
+  config.assets.js_compressor = Uglifier.new(:mangle => false) if defined? Uglifier
 
 end
